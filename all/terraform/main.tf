363c7b0820242bdf9d2a6b273f39a017d7c27a90
EC2 Default User <ec2-user@ip-10-248-3-204.eu-west-2.compute.internal>
2022-05-11 22:25:16 +0000

    all

--- all/terraform/main.tf
+++ all/terraform/main.tf
@@ -1,31 +1,31 @@
 module "S3" {
   source                  = "git::https://github.com/qumarhussain/terraform.git//S3?ref=AWSS3Bucket"
   bucketName              = "${var.bucketName}"
-  acl 			          = "${var.acl}"
+  acl 			              = "${var.acl}"
   projectName             = "${var.projectName}"
-  env 			          = "${var.env}"
+  env 			              = "${var.env}"
 }
 
 module "RDS" {
   source                  = "git::https://github.com/qumarhussain/terraform.git//DBInstance?ref=AWSRDS"
   dbStorage               = "${var.dbStorage}"
-  dbEngine 			      = "${var.dbEngine}"
+  dbEngine 			          = "${var.dbEngine}"
   dbVersion               = "${var.dbVersion}"
-  dbInstance 		      = "${var.dbInstance}"
+  dbInstance 		          = "${var.dbInstance}"
   dbName                  = "${var.dbName}"
-  dbUser 			      = "${var.dbUser}"
+  dbUser 			            = "${var.dbUser}"
   dbPass                  = "${var.dbPass}"
-  brp 			          = "${var.brp}"
+  brp 			              = "${var.brp}"
   family                  = "${var.family}"
   logStatement            = "${var.logStatement}"
   logMinDurationStatement = "${var.logMinDurationStatement}"
-  skip 			          = "${var.skip}"
+  skip 			              = "${var.skip}"
 }
 
 module "EC2" {
   source                  = "git::https://github.com/qumarhussain/terraform.git//EC2?ref=AWSEC2"
-  amiID              	  = "${var.amiID}"
-  instanceType 			  = "${var.instanceType}"
+  amiID              	    = "${var.amiID}"
+  instanceType 			      = "${var.instanceType}"
   projectName             = "${var.projectName}"
-  env 			          = "${var.env}"
+  env 			              = "${var.env}"
 }
